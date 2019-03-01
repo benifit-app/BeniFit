cc0dd8c2b2c2d9fe5028262a34f1e151e136c039
Sandor Menyhay <Sandorm6@hotmail.com>
2019-02-28 16:33:57 -0800

    Updated Activity Locator page/Search Text
    Need to install dependencies

--- lib/pages/activity_locator_page.dart
+++ lib/pages/activity_locator_page.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:fitapp/pages/feed.dart';
-import 'package:fitapp/pages/upload_page.dart';
-import 'dart:async';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fitapp/pages/profile_page.dart';
-import 'package:fitapp/pages/search_page.dart';
-import 'package:fitapp/pages/activity_feed.dart';
-import 'package:fitapp/main/create_account.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'dart:io' show Platform;
+import './pages/moving_markers.dart';
+import './pages/esri.dart';
+import './pages/home.dart';
+import './pages/map_controller.dart';
+import './pages/animated_map_controller.dart';
+import './pages/marker_anchor.dart';
+import './pages/plugin_api.dart';
+import './pages/polyline.dart';
+import './pages/tap_to_add.dart';
+import './pages/offline_map.dart';
+import './pages/on_tap.dart';
+import './pages/circle.dart';
+import './pages/overlay_image.dart';
 
+
+Widget build(BuildContext context) {
+  return new FlutterMap(
+    options: new MapOptions(
+      center: new LatLng(51.5, -0.09),
+      zoom: 13.0,
+    ),
+    layers: [
+      new TileLayerOptions(
+        urlTemplate: "https://api.tiles.mapbox.com/v4/"
+            "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
+        additionalOptions: {
+          'accessToken': '<PUT_ACCESS_TOKEN_HERE>',
+          'id': 'mapbox.streets',
+        },
+      ),
+      new MarkerLayerOptions(
+        markers: [
+          new Marker(
+            width: 80.0,
+            height: 80.0,
+            point: new LatLng(51.5, -0.09),
+            builder: (ctx) =>
+            new Container(
+              child: new FlutterLogo(),
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+}
