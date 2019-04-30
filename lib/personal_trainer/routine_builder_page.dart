d984a63fc3d6571aedab81a36558e4189b262ae2
Alex <Alex@swuser-129-8-237-48.swlan.csufresno.edu>
2019-04-30 16:39:57 -0700

    Routine Cards Complete

--- lib/personal_trainer/routine_builder_page.dart
+++ lib/personal_trainer/routine_builder_page.dart
@@ -2,16 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:fitapp/personal_trainer/menu_Card.dart';
 import 'package:fitapp/personal_trainer/exercise_search.dart';
-import 'package:fitapp/feed/image_post.dart';
+import 'package:fitapp/personal_trainer/routine_builder_search.dart';
 import 'dart:async';
-import 'package:async/async.dart';
-import 'package:fitapp/main.dart';
 import 'dart:io';
-import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:fitapp/feed/floating_action_bar.dart';
-import 'package:fitapp/pages/upload_page.dart';
-import 'package:fitapp/feed/upload_text.dart';
 
 
 class routineBuilderPage extends StatefulWidget{
@@ -74,7 +67,7 @@ class _routineBuilderPage extends State<routineBuilderPage>{
           onTap: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (BuildContext context) => new exerciseSearchPage())
+              MaterialPageRoute(builder: (BuildContext context) => new routineSearchPage())
             );
           },
           child: new menuCard("Routine Search", 250, "assets/images/search.png", BoxFit.cover, Alignment.center, BorderRadius.circular(20)),
---
+++ lib/personal_trainer/routine_builder_search.dart
@@ -0,0 +1,98 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+//
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fitapp/personal_trainer/routine_Card.dart';
+import 'package:flutter/cupertino.dart';
+
+class routineSearchPage extends StatefulWidget {
+_routineSearchPage createState() => new _routineSearchPage();
+}
+
+class _routineSearchPage extends State<routineSearchPage> {
+  Future<QuerySnapshot> queryResults;
+
+  buildSearchField() {
+    return new AppBar(
+      backgroundColor: Colors.white,
+      title: new Form(
+        child: new TextFormField(
+          keyboardType: TextInputType.text,
+          decoration: new InputDecoration(labelText: 'Search for a routine'),
+          onFieldSubmitted: submit,
+        ),
+      ),
+      leading: BackButton(color: Colors.black,),
+      //leading: FlatButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.arrow_back),),
+    );
+  }
+
+  buildSearchFieldIOS(){
+    return new CupertinoNavigationBar(
+      leading: BackButton(color: Colors.black,),
+      middle: new CupertinoTextField(
+        keyboardType: TextInputType.text,
+        clearButtonMode: OverlayVisibilityMode.editing,
+        placeholder: "Search for a routine...",
+        onSubmitted: submit,
+      ),
+    );
+  }
+
+  ListView buildSearchResultsALT(List<DocumentSnapshot> docs) {
+    List<routineCard> userSearchItems = [];
+
+    docs.forEach((DocumentSnapshot doc) {
+      var entry = new routineCard(doc);
+
+      userSearchItems.add(entry);
+    });
+
+    return new ListView(
+      children: userSearchItems.map((w) {
+        return Padding(
+          padding: EdgeInsets.only(top: 15, left: 15, right: 15,),
+          child: w,
+        );
+      }).toList(),
+    );
+  }
+
+  void submit(String searchValue) async {
+    Future<QuerySnapshot> searchFuture = Firestore.instance
+        .collection("Routines")
+    //.where('Exercise_Name',  isGreaterThanOrEqualTo: searchValue[0])
+    //.where('Exercise_Name', isLessThanOrEqualTo: searchValue[fullLength-1])
+        .where('Routine Name', isEqualTo: searchValue)
+        .getDocuments();
+
+    setState(() {
+      queryResults = searchFuture;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: Platform.isIOS ? buildSearchFieldIOS() : buildSearchField(),
+      body: queryResults == null
+          ? new Text("")
+          : new FutureBuilder<QuerySnapshot>(
+          future: queryResults,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              new Text("Snapshot has data");
+              //return buildSearchResults(snapshot.data.documents);
+              return buildSearchResultsALT(snapshot.data.documents);
+            } else {
+              return new Container(
+                  alignment: FractionalOffset.center,
+                  child: Platform.isIOS ? new CupertinoActivityIndicator() : new CircularProgressIndicator());
+            }
+          }
+      ),
+    );
+  }
+}
